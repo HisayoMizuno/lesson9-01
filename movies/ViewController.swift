612a274016891ca137109eaaf77ad6ab143bb9dc
kame.taroui <chunchan0629@gmail.com>
2018-09-10 15:26:06 +0900

    文字が流れるスライドショー です。

--- movies/ViewController.swift
+++ movies/ViewController.swift
@@ -7,24 +7,34 @@
 //
 
 import UIKit
+import Foundation
 import Spring
 
 class ViewController: UIViewController {
     @IBOutlet weak var imgArea: SpringImageView!
-    
+    @IBOutlet weak var boxView: SpringView!
     @IBOutlet weak var timerLabel: UILabel!
-    @IBOutlet weak var textLabel: CBAutoScrollLabel!
+    @IBOutlet weak var textLabel: UILabel!
+    
     
     var timer: Timer?
+    var timer_text: Timer?
     var imag:UIImage!
-    var timer_sec: Float = 0
     var dspimgNo = 0
-    let imgAryname = ["1.jpg","2.jpg","3.jpg","4.jpg"]
+    let imgAryname = ["0.JPG","1.JPG","2.JPG","3.JPG"]
+    var strtext: String = ""
+    var strcount = 0
+    var idx = 0
+    var flgstr = false
+    var timer_sec: Float = 0
+    var text_timer_sec: Float = 0
+    var imag_timer_sec: Float = 0
 
     //-----------------------------------------
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
     }
+  
     //-----------------------------------------
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -33,30 +43,47 @@ class ViewController: UIViewController {
     //-----------------------------------------
     override func viewDidLoad() {
         super.viewDidLoad()
+        /*
+        imgArea.animation = "zoomIn"
+        imgArea.duration = 5
+        imgArea.alpha = 2.0
+        imgArea.delay = 1
+        imgArea.animate()
+         
+        boxView.animation = "slideLeft"
+        boxView.duration = 5
+        boxView.alpha = 2.0
+        boxView.delay = 3
+        boxView.animate()
+ 
+        
         // default表示
         if imag == nil {
-            //AutScroll
-            textLabel.text = "１番目の写真はごえもんくん5歳です"
-            textLabel.labelSpacing = 50;                          // 開始と終了の間間隔
-            textLabel.pauseInterval = 1;                          // スクロール前の一時停止時間
-            textLabel.scrollSpeed = 100.0;                        // スクロール速度
-            textLabel.fadeLength = 20.0;                          // 左端と右端のフェードの長さ
-            textLabel.font = UIFont.systemFont(ofSize: 25.0)      // フォント設定
-            let defimg = UIImage(named: imgAryname[0])
-            imgArea.image = defimg
+            //textLabel.text = "１番目の写真はごえもんくん5歳です"
+            //textLabel.font = UIFont.systemFont(ofSize: 25.0)      // フォント設定
+            //let defimg = UIImage(named: imgAryname[0])
+            //imgArea.image = defimg
+            //textView()
         }
         else{
-            imgArea.image = imag
+            print("bbbbbbbbbbbbbbbbbb")
+
+            //imgArea.image = imag
+
         }
-        //スライド
+       */
+
+        //スライド------------------
         if self.timer == nil {
-            self.timer_sec = 0
-            self.timer = Timer.scheduledTimer(timeInterval: 5.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+            self.timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer(timer:)), userInfo: nil, repeats: true)
+            //self.timer = Timer.scheduledTimer(timeInterval: TimeInterval(timer_sec + 5.0), target: self, selector: #selector(updateTimer(timer:)), userInfo: nil, repeats: true)
         }
         else{
             self.timer?.invalidate() // タイマーを破棄
             self.timer = nil // timerをnilにする(startTimer用）
         }
+
+
     }
 
     //-----------------------------------------
@@ -69,49 +96,73 @@ class ViewController: UIViewController {
     //-----------------------------------------
     func viewImage() {
         
-        //imgArea.animation = "fadeInLeft"
-        //imgArea.duration = 1.0
-        //imgArea.animate()
         if dspimgNo < 0 {
             dspimgNo = 3
         }
         if dspimgNo > 3 {
             dspimgNo = 0
         }
+    
         let name = imgAryname[dspimgNo]
         let img  = UIImage(named: name)
         imgArea.image = img
-        
 
         
-        switch dspimgNo {
-        case 0 :
-            self.textLabel.text = "１番目の写真はごえもんくん5歳です１番目の写真はごえもんくん5歳です"
-        case 1 :
-            self.textLabel.text = "2番目の写真もごえもんくんです2番目の写真もごえもんくんです2番目の写真もごえもんくんです"
-        case 2 :
-            self.textLabel.text = "3番目の写真もごえもんくんです"
-        case 3 :
-            self.textLabel.text = "4番目の写真もごえもんくんです"
-        default:
-            self.textLabel.text = "１番目の写真はごえもんくん5歳です"
-      
-        }
-        //AutScroll
-        textLabel.labelSpacing = 50;                          // 開始と終了の間間隔
-        textLabel.pauseInterval = 1;                          // スクロール前の一時停止時間
-        textLabel.scrollSpeed = 100.0;                         // スクロール速度
-        textLabel.fadeLength = 20.0;                          // 左端と右端のフェードの長さ
-        textLabel.font = UIFont.systemFont(ofSize: 25.0)      // フォント設定
+        imgArea.animation = "zoomIn"
+        imgArea.duration = 5
+        imgArea.alpha = 2.0
+        imgArea.delay = 1
+        imgArea.animate()
+        
     }
-
     //----------------------------------------
-    
-    @objc func updateTimer(timer: Timer){
-        self.timer_sec += 5.0
-        self.timerLabel.text = String(format: "%.1f", timer_sec)
-        upimgNo()
-        viewImage()
+    @objc func textView() {
+        print("++++++textView入り口  dspimgNo = \(dspimgNo) idx=\(idx)")
+        switch dspimgNo {
+            case 1 : strtext = "１番目の写真はごえもんくん5歳です\n１番目の写真はごえもんくん5歳です"
+            case 2 : strtext = "2番目の写真もごえもんくんです"
+            case 3 : strtext = "3番目の写真もごえもんくんです\n3番目の写真もごえもんくんです\n3番目の写真もごえもんくんです"
+            case 4 : strtext = "4番目の写真もごえもんくんです"
+            default: strtext = "１番目の写真はごえもんくん5歳です"
+        }
+        print("***** imgno=\(dspimgNo) / strtext=\(strtext) / idx=\(idx)")
+
+        if idx < strtext.count {
+            //print("aaaaaaa \(strtext)")
+            self.textLabel.text = strtext.substring(to: strtext.index(strtext.startIndex, offsetBy: idx))
+            idx += 1
+            text_timer_sec += 1.0
+            //print("eeeeeeeeeeeeeeeeeee idx==\(idx)  / \(String(describing: textLabel.text))")
+        }
+        textLabel.font = UIFont.systemFont(ofSize: 25.0)        // フォント設定
+    }
+    //----------------------------------------
+    @objc func updateTimer(timer: Timer) {
+
+
+        print("⭐︎updateTime入口   strtext= \(strtext)   text_timer_sec === \(text_timer_sec)")
+        
+        if text_timer_sec == Float(strtext.count)  {
+            viewImage()
+            idx = 0
+            if self.timer_text == nil {
+                timer_text = Timer.scheduledTimer(timeInterval: 0.5 ,target: self, selector: #selector(textView),userInfo: nil,repeats: true)
+                timer_text?.fire()
+                
+            }
+            else{
+                print("222222222222222222222222 END  === \(text_timer_sec)")
+                timer_sec = text_timer_sec + timer_sec
+                text_timer_sec = 0
+                self.timer = nil // timerをnilにする(startTimer用）
+            }
+            self.timerLabel.text = String(format: "%.1f", timer_sec)
+            upimgNo()
+        }
+
+        
+
+        
     }
 }
 
